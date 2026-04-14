1e487c0ad70ee5c0db992d47e4e7ad19b2210cff
yoshika-xr <yoshikajune@gmail.com>
2026-04-14 15:54:48 +0530

    startsidesection.init

---
+++ CGPA-App/src/componant/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Sidebar(){
+    return(
+        <>
+        <div className="first-top">
+        </div>
+        <div className="second-buttom"></div>
+        </>
+    )
+}
+export default Sidebar
