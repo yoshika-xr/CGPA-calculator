916b836817522854a98740d8f323ac1f70182af6
yoshika-xr <yoshikajune@gmail.com>
2026-04-14 15:50:46 +0530

    headerfinish.almost

--- CGPA-App/src/componant/Header.jsx
+++ CGPA-App/src/componant/Header.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import logo from "../assets/logo1.jpg";
 
-function Header(){
-    return(
-        <div className="header">
-            <ul  className="head-cont" type={"none"}>
-                <li>Home</li>
-                <li>Score</li>
-                <li>How to Calc</li>
-                <li>CGPA</li>
-                <li>Sign</li>
-            </ul>
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="left-side">
+          <img className={"logo"} src={logo} alt="logo" />
+
+          <h2 className={"head-title $"}>CGPA Calculator</h2>
         </div>
-    )
+
+        <ul className="head-cont">
+          <li>Home</li>
+          <li>Score</li>
+          <li>How to Calc</li>
+          <li>CGPA</li>
+          <li>Sign</li>
+        </ul>
+      </div>
+      <hr />
+    </>
+  );
 }
 
 export default Header;
