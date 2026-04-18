cd62b574af32bfbcd814a2866b900db96c4921a7
yoshika-xr <yoshikajune@gmail.com>
2026-04-18 22:00:36 +0530

    update calculation section

--- CGPA-App/src/componant/Calculation.jsx
+++ CGPA-App/src/componant/Calculation.jsx
@@ -4,10 +4,13 @@ function Calculation() {
   return (
     <>
       <div className="calcu-sec">
+        <div className="inside-calc">
         <input className="input-form" type="text" placeholder="Enter subject name" />
         <input className="input-form" type="text" placeholder="Enter subject code" />
         <input className="input-form" type="text" placeholder="Enter grade" />
         <input className="input-form" type="number" placeholder="Enter credits" />
+        <i class="bi bi-x-circle"></i>
+        </div>
         <i class="bi bi-plus-circle"></i>
       </div>
     </>
