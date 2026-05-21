dcd7fe3ae81efa0811a03e8674dd61cd6189252c
yoshika-xr <yoshikajune@gmail.com>
2026-05-21 22:33:44 +0530

    update formula section

--- src/componant/Formula.jsx
+++ src/componant/Formula.jsx
@@ -2,7 +2,66 @@ import React from 'react'
 
 function Formula() {
   return (
-    <div>Formula</div>
+    <div>
+      GPA Calculation
+
+GPA (Grade Point Average) is calculated for one semester.
+
+Formula
+
+GPA=
+∑Credits
+∑(Grade Point×Credit)
+	​
+
+
+Steps
+Multiply each subject’s Grade Point × Credit
+Add all the values
+Add total credits
+Divide total grade points by total credits
+Example
+Subject	Grade Point	Credit	GP × Credit
+Maths	9	4	36
+Java	8	3	24
+DBMS	10	3	30
+Total = 36 + 24 + 30 = 90
+Total Credits = 4 + 3 + 3 = 10
+GPA
+
+GPA=
+10
+90
+	​
+
+=9.0
+
+CGPA Calculation
+
+CGPA (Cumulative Grade Point Average) is calculated using all semester GPAs.
+
+Formula
+
+CGPA=
+Number of Semesters
+∑Semester GPAs
+	​
+
+
+Example
+Semester	GPA
+Sem 1	8.5
+Sem 2	9.0
+Sem 3	8.8
+CGPA
+
+CGPA=
+3
+8.5+9.0+8.8
+	​
+
+=8.76
+    </div>
   )
 }
 
